a037bc91868acff7f97ee599dac082f6ee9e8294
sinabin <sinabin@naver.com>
2023-09-08 21:39:17 +0900

    누락된 부분 commit

--- src/main/java/com/example/happyusf/Domain/MobileVerificationCodeDTO.java
+++ src/main/java/com/example/happyusf/Domain/MobileVerificationCodeDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
 import java.sql.Timestamp;
 
 @Data
@@ -12,8 +13,14 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @Builder
 public class MobileVerificationCodeDTO {
+
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "전화번호는 XXX-XXXX-XXXX 형식으로 입력해주세요.")
     private String phone_number;
+
+    @Pattern(regexp = "\\d{6}", message = "인증코드는 6자리입니다.")
     private String sent_code;
+
     private Timestamp sent_time;
+
     private boolean verification_result;
 }
