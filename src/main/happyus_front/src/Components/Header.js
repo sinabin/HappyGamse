7a16bccbec0a39e018277ecfb0e337a7e86724d9
sinabin <sinabin@naver.com>
2023-09-27 13:10:13 +0900

    게임 뉴스 test 페이지

--- src/main/happyus_front/src/Components/Header.js
+++ src/main/happyus_front/src/Components/Header.js
@@ -19,47 +19,44 @@ function Header() {
             });
     }, []);
 
-    if (isLogined === true) {
-        return (
-            <Navbar bg="custom" variant="dark" id="HeaderBar">
-                <Container>
-                    <Link to="/" style={{marginRight: '1%'}}> <img alt="header_logo" style={{display: "inline"}} src="/imgs/header_logo.png"/> </Link>
-                    <Link to="/" style={{textDecoration: 'none'}}><Navbar.Brand>HappyGames</Navbar.Brand> </Link>
+    return (
+        <Navbar bg="custom" variant="dark" id="HeaderBar" expand="lg">
+            <Container>
+                <Link to="/" style={{marginRight: '1%'}}>
+                    <img alt="header_logo" style={{display: "inline"}} src="/imgs/header_logo.png"/>
+                </Link>
+                <Link to="/" style={{textDecoration: 'none'}}>
+                    <Navbar.Brand>HappyGames</Navbar.Brand>
+                </Link>
+
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link><Link to="/news" style={{textDecoration: 'none', color: 'white'}}>게임 뉴스</Link> </Nav.Link>
                         <Nav.Link><Link to="/" style={{textDecoration: 'none', color: 'white'}}>실시간 핫토픽</Link></Nav.Link>
                         <Nav.Link><Link to="/" style={{textDecoration: 'none', color: 'white'}}>이벤트 & 할인 정보</Link></Nav.Link>
                         <Nav.Link><Link to="/" style={{textDecoration: 'none', color: 'white'}}>게임 친구찾기</Link></Nav.Link>
                     </Nav>
-                </Container>
-                <DropdownButton variant="light" title="My Page" id="dropDown" style={{marginRight: '2%'}}>
-                    <Dropdown.Item> <Link to="/myPage" style={{textDecoration: 'none', color: 'black'}}>내정보</Link></Dropdown.Item>
-                    <Dropdown.Item> <Link to="/memberShip" style={{textDecoration: 'none', color: 'black'}}>멤버십 관리</Link></Dropdown.Item>
-                    <Dropdown.Item> <Link to="/MyPost" style={{textDecoration: 'none', color: 'black'}}>내 게시물 관리</Link></Dropdown.Item>
-                    <Dropdown.Item href={"/logout"}>로그아웃 </Dropdown.Item>
-                </DropdownButton>
-            </Navbar>
-        );
-    } else {
-        return (
-            <Navbar bg="custom" variant="dark" id="HeaderBar">
-                <Container>
-                    <Link to="/" style={{marginRight: '1%'}}> <img alt="header_logo" style={{display: "inline"}} src="/imgs/header_logo.png"/> </Link>
-                    <Link to="/" style={{textDecoration: 'none'}}><Navbar.Brand>HappyGames</Navbar.Brand> </Link>
-                    <Nav className="me-auto">
-                        <Nav.Link><Link to="/news" style={{textDecoration: 'none', color: 'white'}}>게임 뉴스</Link> </Nav.Link>
-                        <Nav.Link><Link to="/" style={{textDecoration: 'none', color: 'white'}}>실시간 핫토픽</Link></Nav.Link>
-                        <Nav.Link><Link to="/" style={{textDecoration: 'none', color: 'white'}}>이벤트 & 할인 정보</Link></Nav.Link>
-                        <Nav.Link><Link to="/" style={{textDecoration: 'none', color: 'white'}}>게임 친구찾기</Link></Nav.Link>
-                    </Nav>
-                </Container>
-                <DropdownButton variant="light" title="로그인" id="dropDown" style={{marginRight: '2%'}}>
-                    <Dropdown.Item href={"/loginPage"}>로그인 </Dropdown.Item>
-                    <Dropdown.Item href={"/register/agreement"}>회원가입 </Dropdown.Item>
-                </DropdownButton>
-            </Navbar>
-        );
-    }
+
+                    {isLogined ? (
+                        <DropdownButton variant="light" title="My Page" id="dropDown" style={{marginRight: '2%'}}>
+                            <Dropdown.Item> <Link to="/myPage" style={{textDecoration: 'none', color: 'black'}}>내정보</Link></Dropdown.Item>
+                            <Dropdown.Item> <Link to="/memberShip" style={{textDecoration: 'none', color: 'black'}}>멤버십 관리</Link></Dropdown.Item>
+                            <Dropdown.Item> <Link to="/MyPost" style={{textDecoration: 'none', color: 'black'}}>내 게시물 관리</Link></Dropdown.Item>
+                            <Dropdown.Item href={"/logout"}>로그아웃 </Dropdown.Item>
+                        </DropdownButton>
+                    ) : (
+                        <DropdownButton variant="light" title="로그인" id="dropDown" style={{marginRight: '2%'}}>
+                            <Dropdown.Item href={"/loginPage"}>로그인 </Dropdown.Item>
+                            <Dropdown.Item href={"/register/agreement"}>회원가입 </Dropdown.Item>
+                        </DropdownButton>
+                    )}
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default Header;
--- src/main/happyus_front/src/Pages/NewsPage.js
+++ src/main/happyus_front/src/Pages/NewsPage.js
@@ -1,25 +1,82 @@
-// NewsPage.js
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 function NewsPage() {
     const [newsList, setNewsList] = useState([]);
     const [page, setPage] = useState(1);
 
     useEffect(() => {
-        fetch(`/api/news?page=${page}`)
-            .then(response => response.json())
-            .then(data => setNewsList(data));
+        setNewsList([{
+            id: "1",
+            title: "퍼스트 디센던트, 콘솔과 PC에서 공개 테스트 시작",
+            imgLocation: "/imgs/news/test1.jpg",
+            summary: "넥슨은 19일 16시(한국시간)부터 루트슈터 신작 ‘퍼스트 디센던트(The First Descendant)’의 크로스플레이 공개 테스트를 시작한다. 테스트는 26일까지 진행되며, PC(스팀), Xbox One, Xbox 시리즈..."
+        }, {
+            id: "2",
+            title: "P의 거짓, 잔혹동화와 강렬한 액션 담긴 런칭 트레일러 공개",
+            imgLocation: "/imgs/news/test2.jpg",
+            summary: "네오위즈는 19일, 산하 스튜디오 라운드8의 소울라이크 신작 P의 거짓(Lies of P) 출시에 맞춰 런칭 트레일러를 공개했다. P의 거짓 공식 유튜브 채널을 통해 공개된 런칭..."
+        },{
+            id: "3",
+            title: "P의 거짓, 잔혹동화와 강렬한 액션 담긴 런칭 트레일러 공개",
+            imgLocation: "/imgs/news/test2.jpg",
+            summary: "네오위즈는 19일, 산하 스튜디오 라운드8의 소울라이크 신작 P의 거짓(Lies of P) 출시에 맞춰 런칭 트레일러를 공개했다. P의 거짓 공식 유튜브 채널을 통해 공개된 런칭..."
+        },{
+            id: "4",
+            title: "P의 거짓, 잔혹동화와 강렬한 액션 담긴 런칭 트레일러 공개",
+            imgLocation: "/imgs/news/test2.jpg",
+            summary: "네오위즈는 19일, 산하 스튜디오 라운드8의 소울라이크 신작 P의 거짓(Lies of P) 출시에 맞춰 런칭 트레일러를 공개했다. P의 거짓 공식 유튜브 채널을 통해 공개된 런칭..."
+        },{
+            id: "5",
+            title: "P의 거짓, 잔혹동화와 강렬한 액션 담긴 런칭 트레일러 공개",
+            imgLocation: "/imgs/news/test2.jpg",
+            summary: "네오위즈는 19일, 산하 스튜디오 라운드8의 소울라이크 신작 P의 거짓(Lies of P) 출시에 맞춰 런칭 트레일러를 공개했다. P의 거짓 공식 유튜브 채널을 통해 공개된 런칭..."
+        },{
+            id: "6",
+            title: "P의 거짓, 잔혹동화와 강렬한 액션 담긴 런칭 트레일러 공개",
+            imgLocation: "/imgs/news/test2.jpg",
+            summary: "네오위즈는 19일, 산하 스튜디오 라운드8의 소울라이크 신작 P의 거짓(Lies of P) 출시에 맞춰 런칭 트레일러를 공개했다. P의 거짓 공식 유튜브 채널을 통해 공개된 런칭..."
+        }])
     }, [page]);
 
     return (
-        <div>
-            <span>뉴스 페이지</span>
-{/*            {newsList.map((news) => (
-                <div key={news.id}>
-                    <h2>{news.title}</h2>
-                    <p>{news.summary}</p>
+        <div style={{
+            backgroundColor: "#333",
+            padding: "20px",
+            fontFamily: "'Arial', sans-serif",
+            maxWidth: "600px",
+            margin: "0 auto"
+        }}>
+            <h1 style={{
+                color: "white",
+                borderBottom: "solid 3px #808000",
+                textAlign: "center",
+                padding: "20px",
+                fontSize: window.innerWidth <= 768 ? '1.5em' : '2em',
+                fontWeight: "bold",
+                textShadow: '2px 2px 4px #000000',
+            }}> Game News</h1>
+
+            {newsList.map((news) => (
+                <div key={news.id} style={{
+                    backgroundColor: "#666",
+                    borderRadius: "10px",
+                    padding: "20px",
+                    marginBottom: "20px",
+                    display: 'flex',
+                    flexDirection: window.innerWidth <= 768 ? 'column' : 'row'
+                }}>
+                    <img src={news.imgLocation} alt="News" style={{
+                        width: window.innerWidth <= 768 ? '100%' : "200px",
+                        height: "auto",
+                        marginRight: window.innerWidth <= 768 ? '0' : "20px",
+                        marginBottom: window.innerWidth <= 768 ? '20px' : "0"
+                    }}/>
+                    <div>
+                        <h2 style={{color: "white", fontWeight: "bold"}}>{news.title}</h2>
+                        <p style={{color: "white"}}>{news.summary}</p>
+                    </div>
                 </div>
-            ))}*/}
+            ))}
 
             {page > 1 &&
                 <button onClick={() => setPage(page - 1)}>이전 페이지</button>}
